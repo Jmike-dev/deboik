0face416efe5b4ef4e6aea67be4b7c7ca1e48add
Jmike-dev <gmikekamau@gmail.com>
2025-03-11 02:53:43 +0300

    feat:user creation is now working

---
+++ app/services/apiService.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+const BASE_URL = "http://localhost:3000/api/deboikStaff";
+
+interface User {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    phone?: string;
+    role?: string;
+}
+export async function createUser(params: User) {
+    try {
+        const response = await axios.post(BASE_URL, params);
+        return response.data;
+    } catch (error) {
+        console.error("Couldn't create user", error);
+        return null;
+    }
+}
