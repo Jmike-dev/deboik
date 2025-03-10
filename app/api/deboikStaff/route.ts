215e5979d2b624af44c6398987584847f0ae723a
Jmike-dev <gmikekamau@gmail.com>
2025-03-10 10:53:29 +0300

    feat:post api created and working

---
+++ app/api/deboikStaff/route.ts
@@ -0,0 +1,28 @@
+import connectMongoDb from "@/app/lib/mongodb";
+import deboikStaff from "@/app/models/deboikStaff";
+import { NextRequest, NextResponse } from "next/server";
+export async function POST(request: NextRequest) {
+    interface User {
+        firstName: string;
+        lastName: string;
+        email: string;
+        password: string;
+        phone: string;
+        role: string; 
+    }
+    const { firstName, lastName, email, password, phone, role }:User =
+        await request.json();
+    await connectMongoDb();
+    await deboikStaff.create({
+        firstName,
+        lastName,
+        email,
+        password,
+        phone,
+        role,
+    });
+    return NextResponse.json(
+        { message: "deboik staff created" },
+        { status: 201 },
+    );
+}
--- app/lib/mongodb.ts
+++ app/lib/mongodb.ts
@@ -1,40 +1,12 @@
-// lib/mongodb.js
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI as string;
-
-if (!MONGODB_URI) {
-    throw new Error("Please define the MONGODB_URI environment variable");
-}
-
-
-declare global {
-    var mongoose:
-        | {
-              conn: mongoose.Mongoose | null;
-              promise: Promise<mongoose.Mongoose> | null;
-          }
-        | undefined;
-}
-
-// Use global cache if available, otherwise initialize it
-let cached = global.mongoose as any;
-
-if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function dbConnect() {
-    if (cached.conn) {
-        return cached.conn;
+const connectMongoDb = async (): Promise<void> => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL as string);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
     }
+};
 
-    if (!cached.promise) {
-        cached.promise = mongoose.connect(MONGODB_URI).then((m) => m);
-    }
-
-    cached.conn = await cached.promise;
-    return cached.conn;
-}
-
-export default dbConnect;
+export default connectMongoDb;
